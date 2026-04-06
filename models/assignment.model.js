b5a6a0fdf4bdec1f72acfdf7de612919c2018cb3
dev11nexcore <dev11.nexcore@gmail.com>
2026-04-06 14:39:21 +0530

    refactor assignment model schema formatting

--- models/assignment.model.js
+++ models/assignment.model.js
@@ -1,19 +1,23 @@
 import mongoose from "mongoose";
 
 const assignmentSchema = new mongoose.Schema({
-    batchName: String,
-    batchNumber: Number,
-    student: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
-    parameters: [
-        {
-            name: String,
-            score: Number,
-        },
-    ],
-    comment: {
-  type: String,
-  default: "",
-},
+  batchName: String,
+  batchNumber: Number,
+  student: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
+  parameters: [
+    {
+      name: String,
+      score: Number,
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  comment: {
+    type: String,
+    default: "",
+  },
 });
 
 export default mongoose.model("Assignment", assignmentSchema);
